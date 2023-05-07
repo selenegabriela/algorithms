c8a6a20870e301146e2dc02a8954f67f12a7a251
Selene Gabriela Amador Díaz <sel_nn@hotmail.com>
2023-05-07 06:31:19 -0600

    hash table

--- dataStructure.js
+++ dataStructure.js
@@ -628,17 +628,63 @@ class DoubleLinkedList {
 
 const doubleList = new DoubleLinkedList()
 
-doubleList.append(1)
-doubleList.prepend(2)
-doubleList.append(3)
-doubleList.append(4)
-console.log(doubleList.removeFromFront())
-console.log(doubleList.removeFromFront())
-console.log(doubleList.removeFromFront())
-console.log(doubleList.removeFromFront())
+// doubleList.append(1)
+// doubleList.prepend(2)
+// doubleList.append(3)
+// doubleList.append(4)
+// console.log(doubleList.removeFromFront())
+// console.log(doubleList.removeFromFront())
+// console.log(doubleList.removeFromFront())
+// console.log(doubleList.removeFromFront())
 
+// doubleList.printFromFront()
+// doubleList.printFromEnd()
 
+// HASH TABLE
 
-doubleList.printFromFront()
-doubleList.printFromEnd()
+class HashTable {
+    constructor(size){
+        this.table = new Array(size);
+        this.size = size;
+    }
 
+    hash(key){
+        let total = 0;
+        for(let i = 0; i < key.length; i++){
+            total += key.charCodeAt(i);
+        }
+        return total % this.size;
+    }
+
+    set(key, value){
+        const index = this.hash(key);
+        this.table[index] = value;
+    }
+
+    get(key){
+        const index = this.hash(key);
+        return this.table[index];
+    }
+
+    remove(key) {
+        const index = this.hash(key);
+        const value = this.table[index];
+        this.table[index] = undefined;
+        return value;
+    }
+
+    display(){
+        for(let i = 0; i < this.table.length; i++){
+            if(this.table[i]){
+                console.log(`${i}: ${this.table[i]}`)
+            }
+        }
+    }
+}
+
+const table = new HashTable(50);
+
+table.set('name', 'Mazapán');
+table.set('age', 5);
+console.log(table.get('age'));
+table.display()
